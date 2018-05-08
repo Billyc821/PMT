0e2bd5525010d7a4c2bcd389e62a39aaeecec0e4
Billy <billy@cherrett.digital>
2018-05-08 10:20:29 +0100

    Added view file

---
+++ view.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="main.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="myscripts.js"></script>
+    <title>CorexSmart</title>
+
+  </head>
+  <body>
+
+    <nav>
+        <p>CorexSmart</p>
+    </nav>
+
+    <div class="main_container3">
+
+
+      <div class="item-one">
+
+        <div class="icon-wrapper">
+
+          <i class="material-icons add">add_circle</i>
+
+        </div>
+
+        <h1 class="device_title">Device name</h1>
+
+      </div>
+
+      <div class="item-two">
+
+      </div>
+
+
+
+
+
+    </div>
+
+  </body>
+</html>
